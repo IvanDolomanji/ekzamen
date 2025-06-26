69aba82ba013b8d742ac3254b55b7abe2fe71453
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2025-06-26 13:37:51 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запрашиваем у пользователя длины сторон
+            Console.WriteLine("Введите длину первой стороны:");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите длину второй стороны:");
+            double side2 = Convert.ToDouble(Console.ReadLine());
+
+            // Создаем новый экземпляр класса Rectangle с введенными данными
+            Rectangle rectangle = new Rectangle(side1, side2);
+
+            // Выводим площадь и периметр
+            Console.WriteLine($"Площадь прямоугольника: {rectangle.Area}");
+            Console.WriteLine($"Периметр прямоугольника: {rectangle.Perimeter}");
+
+            // Ожидаем нажатия клавиши для закрытия консоли
+            Console.ReadLine();
+        }
+    }
+}
+
+   
+    
+
---
+++ ConsoleApp1/Rectangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Rectangle
+    {
+        // Приватные поля для хранения длин сторон
+        private double side1;
+        private double side2;
+
+        // Конструктор для инициализации сторон
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+        // Метод для вычисления площади (side1 * side2)
+        public double AreaCalculator()
+        {
+            return side1 * side2;
+        }
+
+        // Метод для вычисления периметра (2 * (side1 + side2))
+        public double PerimeterCalculator()
+        {
+            return 2 * (side1 + side2);
+        }
+
+        // Свойство для получения площади
+        public double Area
+        {
+            get { return AreaCalculator(); }
+        }
+
+        // Свойство для получения периметра
+        public double Perimeter
+        {
+            get { return PerimeterCalculator(); }
+        }
+    }
+}
+
