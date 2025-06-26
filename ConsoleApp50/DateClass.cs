bdaa5268aee49c13a7876a23f6ebee105be07f26
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2025-06-26 22:47:52 +0300

    Add files via upload

---
+++ ConsoleApp50/DateClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp50
+{
+    class DateClass
+    {
+        private DateTime data;
+
+        // Конструктор без параметров (устанавливает текущую дату)
+        public DateClass()
+        {
+            data = DateTime.Now;
+        }
+
+        // Конструктор с тремя параметрами
+        public DateClass(int year, int month, int day)
+        {
+            try
+            {
+                data = new DateTime(year, month, day);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                throw new ArgumentException("Некорректная дата! Проверьте год, месяц и день.");
+            }
+        }
+
+        // Свойство для доступа к полю
+        public DateTime Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        // Метод для вычисления предыдущего дня
+        public DateTime GetPreviousDay()
+        {
+            return data.AddDays(-1);
+        }
+
+        // Метод для вычисления следующего дня
+        public DateTime GetNextDay()
+        {
+            return data.AddDays(1);
+        }
+    }
+}
---
+++ ConsoleApp50/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запрашиваем дату для создания объекта
+            Console.WriteLine("Введите дату:");
+            Console.WriteLine("Год (например, 2025):");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Месяц (1-12):");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("День (1-31):");
+            int day = int.Parse(Console.ReadLine());
+
+            // Создаем объект с введенной датой
+            DateClass date = new DateClass(year, month, day);
+
+            // Выводим исходную дату
+            Console.WriteLine("\nИсходная дата:");
+            Console.WriteLine(date.Data.ToShortDateString());
+
+            // Вычисляем и выводим предыдущий день
+            DateTime previousDay = date.GetPreviousDay();
+            Console.WriteLine("Предыдущий день:");
+            Console.WriteLine(previousDay.ToShortDateString());
+
+            // Вычисляем и выводим следующий день
+            DateTime nextDay = date.GetNextDay();
+            Console.WriteLine("Следующий день:");
+            Console.WriteLine(nextDay.ToShortDateString());
+
+            // Ожидаем нажатия клавиши для закрытия консоли
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadLine();
+        }
+    }
+}
