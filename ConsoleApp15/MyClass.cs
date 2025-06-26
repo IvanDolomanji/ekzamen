e18f86468ea5a0d9e1389a5c70fb982b14bb7572
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2025-06-26 15:47:51 +0300

    Add files via upload

---
+++ ConsoleApp15/MyClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class MyClass : InterfaceC
+    {
+        public void ShowInterfaceA()
+        {
+            Console.WriteLine("Exploring interfaces, example 2...");
+        }
+
+        public void ShowInterfaceB()
+        {
+            Console.WriteLine("Concept of multiple inheritance through interface...");
+        }
+
+        public void ShowInterfaceC()
+        {
+            Console.WriteLine("ShowInterfaceA() is completed.");
+            Console.WriteLine("ShowInterfaceB() is completed.");
+        }
+    }
+}
---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем объект класса MyClass
+            MyClass myClassOb = new MyClass();
+
+            // Вызываем методы для вывода информации
+            myClassOb.ShowInterfaceA();
+            myClassOb.ShowInterfaceB();
+            myClassOb.ShowInterfaceC();
+
+            // Ожидаем нажатия клавиши для закрытия консоли
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadLine();
+        }
+    }
+
+    // Интерфейс InterfaceA
+    interface InterfaceA
+    {
+        void ShowInterfaceA();
+    }
+
+    // Интерфейс InterfaceB
+    interface InterfaceB
+    {
+        void ShowInterfaceB();
+    }
+
+    // Интерфейс InterfaceC с множественным наследованием
+    interface InterfaceC : InterfaceA, InterfaceB
+    {
+        void ShowInterfaceC();
+    }
+}
