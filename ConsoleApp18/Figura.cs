43bdc6603710fa0607a4586c05b8a5f5e26a060e
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2025-06-26 15:54:39 +0300

    Add files via upload

---
+++ ConsoleApp18/Figura.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Figura
+    {
+        public void ShowArea(double side)
+        {
+            double area = side * side;
+            Console.WriteLine($"Фигура: Квадрат");
+            Console.WriteLine($"Площадь: {area:F2} единиц²");
+        }
+
+        public void ShowArea(double width, double height)
+        {
+            double area = width * height;
+            Console.WriteLine($"Фигура: Прямоугольник");
+            Console.WriteLine($"Площадь: {area:F2} единиц²");
+        }
+
+        public void ShowArea(double a, double b, double h)
+        {
+            double area = (a + b) * h / 2;
+            Console.WriteLine($"Фигура: Трапеция");
+            Console.WriteLine($"Площадь: {area:F2} единиц²");
+        }
+    }
+}
---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figura figura = new Figura();
+
+            while (true)
+            {
+                // Запрашиваем у пользователя количество параметров
+                Console.WriteLine("\nВыберите фигуру (введите количество сторон):");
+                Console.WriteLine("1. Квадрат (1 параметр)");
+                Console.WriteLine("2. Прямоугольник (2 параметра)");
+                Console.WriteLine("3. Трапеция (3 параметра)");
+                Console.WriteLine("4. Выход");
+                Console.Write("Ваш выбор (1-4): ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Введите длину стороны квадрата:");
+                        double side = double.Parse(Console.ReadLine());
+                        figura.ShowArea(side);
+                        break;
+                    case "2":
+                        Console.WriteLine("Введите длину первой стороны прямоугольника:");
+                        double width = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Введите длину второй стороны прямоугольника:");
+                        double height = double.Parse(Console.ReadLine());
+                        figura.ShowArea(width, height);
+                        break;
+                    case "3":
+                        Console.WriteLine("Введите длину первой параллельной стороны трапеции:");
+                        double a = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Введите длину второй параллельной стороны трапеции:");
+                        double b = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Введите высоту трапеции:");
+                        double h = double.Parse(Console.ReadLine());
+                        figura.ShowArea(a, b, h);
+                        break;
+                    case "4":
+                        Console.WriteLine("Программа завершена.");
+                        return;
+                    default:
+                        Console.WriteLine("Неверный выбор. Попробуйте снова.");
+                        break;
+                }
+            }
+        }
+    }
+}
