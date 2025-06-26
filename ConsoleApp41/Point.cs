b7a4ddf0529ddee2cd6b92e279a8d1c731469518
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2025-06-26 18:34:23 +0300

    Add files via upload

---
+++ ConsoleApp41/Point.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp41
+{
+    class Point<T> where T : struct
+    {
+        private T x;
+        private T y;
+
+        public Point(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public T X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public T Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Координаты точки: ({x}, {y})");
+        }
+    }
+}
---
+++ ConsoleApp41/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание точек с разными типами
+            Console.WriteLine("Создание точки с типом int:");
+            Console.WriteLine("Введите координату x:");
+            int xInt = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату y:");
+            int yInt = int.Parse(Console.ReadLine());
+            Point<int> pointInt = new Point<int>(xInt, yInt);
+            pointInt.Display();
+
+            Console.WriteLine("\nСоздание точки с типом uint:");
+            Console.WriteLine("Введите координату x:");
+            uint xUInt = uint.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату y:");
+            uint yUInt = uint.Parse(Console.ReadLine());
+            Point<uint> pointUInt = new Point<uint>(xUInt, yUInt);
+            pointUInt.Display();
+
+            Console.WriteLine("\nСоздание точки с типом double:");
+            Console.WriteLine("Введите координату x:");
+            double xDouble = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату y:");
+            double yDouble = double.Parse(Console.ReadLine());
+            Point<double> pointDouble = new Point<double>(xDouble, yDouble);
+            pointDouble.Display();
+
+            // Ожидаем нажатия клавиши для закрытия консоли
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadLine();
+        }
+    }
+}
