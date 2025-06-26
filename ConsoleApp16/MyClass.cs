d6e89034fac578b125b61a811ded0e6d92303ad0
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2025-06-26 15:51:07 +0300

    Add files via upload

---
+++ ConsoleApp16/MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class MyClass : ICalculator
+    {
+        public double GetPi()
+        {
+            return Math.PI;
+        }
+
+        public int GetRandomInt()
+        {
+            Random random = new Random();
+            return random.Next();
+        }
+
+        public double Square(double x)
+        {
+            return x * x;
+        }
+
+        public double SquareRoot(double x)
+        {
+            return Math.Sqrt(x);
+        }
+    }
+}
---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запрашиваем у пользователя значение x
+            Console.WriteLine("Введите значение x:");
+            double x = double.Parse(Console.ReadLine());
+
+            // Создаем экземпляр класса MyClass
+            MyClass myClass = new MyClass();
+
+            // Вызываем методы и выводим результаты
+            Console.WriteLine("\nРезультаты вычислений:");
+            Console.WriteLine($"Число PI: {myClass.GetPi()}");
+            Console.WriteLine($"Случайное число типа int: {myClass.GetRandomInt()}");
+            Console.WriteLine($"x в квадрате: {myClass.Square(x)}");
+            Console.WriteLine($"Корень квадратный из x: {myClass.SquareRoot(x)}");
+
+            // Ожидаем нажатия клавиши для закрытия консоли
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadLine();
+        }
+    }
+
+    // Интерфейс с методами
+    interface ICalculator
+    {
+        double GetPi();
+        int GetRandomInt();
+        double Square(double x);
+        double SquareRoot(double x);
+    }
+}
