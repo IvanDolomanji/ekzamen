0ca1fd9574aa08e3ab374e6061c37777e7ff6dd8
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2025-06-26 18:25:50 +0300

    Add files via upload

---
+++ ConsoleApp37/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запрашиваем данные для трех треугольников
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($"\nВведите данные для треугольника {i}:");
+                Console.WriteLine("Сторона a:");
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine("Сторона b:");
+                double b = double.Parse(Console.ReadLine());
+                Console.WriteLine("Сторона c:");
+                double c = double.Parse(Console.ReadLine());
+
+                // Создаем делегат для каждого треугольника
+                TriangleAreaDelegate calcArea = TriangleArea.S;
+                double area = calcArea(a, b, c);
+
+                Console.WriteLine($"Площадь треугольника {i}: {area:F2} кв. единиц");
+            }
+
+            // Ожидаем нажатия клавиши для закрытия консоли
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp37/TriangleArea.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+    static class TriangleArea
+    {
+        public static double S(double a, double b, double c)
+        {
+            // Проверка существования треугольника (теорема о неравенстве треугольника)
+            if (a + b <= c || b + c <= a || a + c <= b)
+                throw new ArgumentException("Треугольник с такими сторонами не существует!");
+
+            // Формула Герона
+            double p = (a + b + c) / 2; // Полупериметр
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+
+    // Делегат, соответствующий методу S
+    delegate double TriangleAreaDelegate(double a, double b, double c);
+}
