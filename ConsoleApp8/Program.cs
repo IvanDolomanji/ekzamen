1860551bd1aa18562afc11033ce3bc8cf216a059
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2025-06-26 14:31:57 +0300

    Add files via upload

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запрашиваем у пользователя данные
+            Console.WriteLine("Введите логин:");
+            string login = Console.ReadLine();
+
+            Console.WriteLine("Введите имя:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Введите фамилию:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Введите возраст:");
+            int age = int.Parse(Console.ReadLine());
+
+            // Создаем экземпляр класса User
+            User user = new User(login, firstName, lastName, age);
+
+            // Выводим информацию о пользователе
+            Console.WriteLine("\nИнформация о пользователе:");
+            Console.WriteLine($"Логин: {user.Login}");
+            Console.WriteLine($"Имя: {user.FirstName}");
+            Console.WriteLine($"Фамилия: {user.LastName}");
+            Console.WriteLine($"Возраст: {user.Age}");
+            Console.WriteLine($"Дата заполнения анкеты: {user.RegistrationDate:dd.MM.yyyy HH:mm:ss}");
+
+            // Ожидаем нажатия клавиши для закрытия консоли
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp8/User.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class User
+    {
+        private string login;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private readonly DateTime registrationDate;
+
+        // Конструктор для инициализации полей
+        public User(string login, string firstName, string lastName, int age)
+        {
+            this.login = login;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.registrationDate = DateTime.Now; // Устанавливаем текущую дату и время
+        }
+
+        // Свойство для логина
+        public string Login
+        {
+            get { return login; }
+            set { login = value; }
+        }
+
+        // Свойство для имени
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        // Свойство для фамилии
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        // Свойство для возраста
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        // Свойство для даты заполнения анкеты (только чтение)
+        public DateTime RegistrationDate
+        {
+            get { return registrationDate; }
+        }
+    }
+}
