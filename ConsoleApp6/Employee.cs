7e4800056a4e18a86a217ae665829b90bc6051c3
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2025-06-26 14:12:36 +0300

    Add files via upload

---
+++ ConsoleApp6/Employee.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Employee
+    {
+        private string lastName;
+        private string firstName;
+
+        // Конструктор для инициализации фамилии и имени
+        public Employee(string lastName, string firstName)
+        {
+            this.lastName = lastName;
+            this.firstName = firstName;
+        }
+
+        // Свойство для получения фамилии
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        // Свойство для получения имени
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        // Метод для получения названия должности
+        public string GetPositionName(string positionInput)
+        {
+            switch (positionInput)
+            {
+                case "1":
+                    return "Junior";
+                case "2":
+                    return "Middle";
+                case "3":
+                    return "Senior";
+                default:
+                    return "Неизвестная должность";
+            }
+        }
+
+        // Метод для вычисления оклада и налогового сбора
+        public (double salary, double tax) CalculateSalaryAndTax(string positionInput, double experience)
+        {
+            double baseSalary;
+
+            // Определяем базовый оклад в зависимости от должности
+            switch (positionInput)
+            {
+                case "1": // Junior
+                    baseSalary = 1000;
+                    break;
+                case "2": // Middle
+                    baseSalary = 2000;
+                    break;
+                case "3": // Senior
+                    baseSalary = 3000;
+                    break;
+                default:
+                    baseSalary = 500; // На случай некорректного ввода
+                    break;
+            }
+
+            // Увеличиваем оклад на 10% за каждый год стажа
+            double salary = baseSalary * (1 + 0.1 * experience);
+
+            // Налоговый сбор 13% от оклада
+            double tax = salary * 0.13;
+
+            return (salary, tax);
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запрашиваем у пользователя данные о сотруднике
+            Console.WriteLine("Введите фамилию сотрудника:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Введите имя сотрудника:");
+            string firstName = Console.ReadLine();
+
+            // Запрашиваем должность сотрудника
+            Console.WriteLine("Введите должность сотрудника (1 - Junior, 2 - Middle, 3 - Senior):");
+            string positionInput = Console.ReadLine();
+
+            // Запрашиваем стаж сотрудника
+            Console.WriteLine("Введите стаж сотрудника (в годах):");
+            double experience = double.Parse(Console.ReadLine());
+
+            // Создаем экземпляр класса Employee
+            Employee employee = new Employee(lastName, firstName);
+
+            // Вычисляем оклад и налоговый сбор
+            (double salary, double tax) = employee.CalculateSalaryAndTax(positionInput, experience);
+
+            // Выводим информацию о сотруднике
+            Console.WriteLine("\nИнформация о сотруднике:");
+            Console.WriteLine($"Фамилия: {employee.LastName}");
+            Console.WriteLine($"Имя: {employee.FirstName}");
+            Console.WriteLine($"Должность: {employee.GetPositionName(positionInput)}");
+            Console.WriteLine($"Оклад: {salary:F2} BYN");
+            Console.WriteLine($"Налоговый сбор (13%): {tax:F2} BYN");
+
+            // Ожидаем нажатия клавиши для закрытия консоли
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadLine();
+        }
+    }
+}
