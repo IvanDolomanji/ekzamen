b1411722acfcdc2377b758ea8ca2a75f9584384e
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2025-06-26 18:32:17 +0300

    Add files via upload

---
+++ ConsoleApp40/AverageCalc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp40
+{
+    static class AverageCalc
+    {
+        public static double Average(int m, int n)
+        {
+            if (m > n)
+                throw new ArgumentException("m не может быть больше n!");
+
+            int sum = 0;
+            int count = 0;
+            for (int i = m; i <= n; i++)
+            {
+                sum += i;
+                count++;
+            }
+            return (double)sum / count;
+        }
+    }
+
+    // Делегат, соответствующий методу Average
+    delegate double AverageDelegate(int m, int n);
+}
---
+++ ConsoleApp40/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запрашиваем данные для трех диапазонов
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($"\nВведите данные для диапазона {i}:");
+                Console.WriteLine("Начальное число m:");
+                int m = int.Parse(Console.ReadLine());
+                Console.WriteLine("Конечное число n:");
+                int n = int.Parse(Console.ReadLine());
+
+                // Проверяем, чтобы m не было больше n
+                if (m > n)
+                {
+                    Console.WriteLine("Ошибка: m не может быть больше n!");
+                    i--; // Повторяем ввод для этого диапазона
+                    continue;
+                }
+
+                // Создаем делегат для каждого диапазона
+                AverageDelegate calcAverage = AverageCalc.Average;
+                double avg = calcAverage(m, n);
+
+                Console.WriteLine($"Среднее арифметическое чисел от {m} до {n}: {avg:F2}");
+            }
+
+            // Ожидаем нажатия клавиши для закрытия консоли
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadLine();
+        }
+    }
+}
