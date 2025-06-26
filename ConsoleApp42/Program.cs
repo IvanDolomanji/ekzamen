db4d6596c4d82f4215b0545552b6fe5555fa9fbf
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2025-06-26 18:36:33 +0300

    Add files via upload

---
+++ ConsoleApp42/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание треугольников с разными типами
+            Console.WriteLine("Создание треугольника с типом int:");
+            Console.WriteLine("Введите сторону a:");
+            int aInt = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите сторону b:");
+            int bInt = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите сторону c:");
+            int cInt = int.Parse(Console.ReadLine());
+            Triangle<int> triangleInt = new Triangle<int>(aInt, bInt, cInt);
+            triangleInt.DisplayInfo();
+            Console.WriteLine($"Площадь: {triangleInt.CalculateArea():F2}");
+
+            Console.WriteLine("\nСоздание треугольника с типом uint:");
+            Console.WriteLine("Введите сторону a:");
+            uint aUInt = uint.Parse(Console.ReadLine());
+            Console.WriteLine("Введите сторону b:");
+            uint bUInt = uint.Parse(Console.ReadLine());
+            Console.WriteLine("Введите сторону c:");
+            uint cUInt = uint.Parse(Console.ReadLine());
+            Triangle<uint> triangleUInt = new Triangle<uint>(aUInt, bUInt, cUInt);
+            triangleUInt.DisplayInfo();
+            Console.WriteLine($"Площадь: {triangleUInt.CalculateArea():F2}");
+
+            Console.WriteLine("\nСоздание треугольника с типом double:");
+            Console.WriteLine("Введите сторону a:");
+            double aDouble = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите сторону b:");
+            double bDouble = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите сторону c:");
+            double cDouble = double.Parse(Console.ReadLine());
+            Triangle<double> triangleDouble = new Triangle<double>(aDouble, bDouble, cDouble);
+            triangleDouble.DisplayInfo();
+            Console.WriteLine($"Площадь: {triangleDouble.CalculateArea():F2}");
+
+            // Ожидаем нажатия клавиши для закрытия консоли
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp42/Triangle.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp42
+{
+    class Triangle<T> where T : struct
+    {
+        private T a;
+        private T b;
+        private T c;
+
+        public Triangle(T a, T b, T c)
+        {
+            // Логика проверки существования треугольника (теорема о неравенстве треугольника)
+            dynamic da = Convert.ToDouble(a);
+            dynamic db = Convert.ToDouble(b);
+            dynamic dc = Convert.ToDouble(c);
+            if (da + db <= dc || db + dc <= da || da + dc <= db)
+                throw new ArgumentException("Треугольник с такими сторонами не существует!");
+
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public T A
+        {
+            get { return a; }
+            set
+            {
+                dynamic da = Convert.ToDouble(value);
+                dynamic db = Convert.ToDouble(b);
+                dynamic dc = Convert.ToDouble(c);
+                if (da + db <= dc || db + dc <= da || da + dc <= db)
+                    throw new ArgumentException("Невозможно задать сторону a: треугольник не существует!");
+                a = value;
+            }
+        }
+
+        public T B
+        {
+            get { return b; }
+            set
+            {
+                dynamic da = Convert.ToDouble(a);
+                dynamic db = Convert.ToDouble(value);
+                dynamic dc = Convert.ToDouble(c);
+                if (da + db <= dc || db + dc <= da || da + dc <= db)
+                    throw new ArgumentException("Невозможно задать сторону b: треугольник не существует!");
+                b = value;
+            }
+        }
+
+        public T C
+        {
+            get { return c; }
+            set
+            {
+                dynamic da = Convert.ToDouble(a);
+                dynamic db = Convert.ToDouble(b);
+                dynamic dc = Convert.ToDouble(value);
+                if (da + db <= dc || db + dc <= da || da + dc <= db)
+                    throw new ArgumentException("Невозможно задать сторону c: треугольник не существует!");
+                c = value;
+            }
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Стороны треугольника: a = {a}, b = {b}, c = {c}");
+        }
+
+        public double CalculateArea()
+        {
+            // Формула Герона
+            dynamic da = Convert.ToDouble(a);
+            dynamic db = Convert.ToDouble(b);
+            dynamic dc = Convert.ToDouble(c);
+            dynamic p = (da + db + dc) / 2;
+            return Math.Sqrt(Convert.ToDouble(p * (p - da) * (p - db) * (p - dc)));
+        }
+    }
+}
