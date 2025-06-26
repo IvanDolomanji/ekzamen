17886da657bdb0cf6a97b21a87079ac9581354db
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2025-06-26 18:38:24 +0300

    Add files via upload

---
+++ ConsoleApp43/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание отрезков с разными типами
+            Console.WriteLine("Создание отрезка с типом int:");
+            Console.WriteLine("Введите x1:");
+            int x1Int = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите y1:");
+            int y1Int = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите x2:");
+            int x2Int = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите y2:");
+            int y2Int = int.Parse(Console.ReadLine());
+            Segment<int> segmentInt = new Segment<int>(x1Int, y1Int, x2Int, y2Int);
+            segmentInt.DisplayInfo();
+            Console.WriteLine($"Длина отрезка: {segmentInt.CalculateLength():F2}");
+
+            Console.WriteLine("\nСоздание отрезка с типом long:");
+            Console.WriteLine("Введите x1:");
+            long x1Long = long.Parse(Console.ReadLine());
+            Console.WriteLine("Введите y1:");
+            long y1Long = long.Parse(Console.ReadLine());
+            Console.WriteLine("Введите x2:");
+            long x2Long = long.Parse(Console.ReadLine());
+            Console.WriteLine("Введите y2:");
+            long y2Long = long.Parse(Console.ReadLine());
+            Segment<long> segmentLong = new Segment<long>(x1Long, y1Long, x2Long, y2Long);
+            segmentLong.DisplayInfo();
+            Console.WriteLine($"Длина отрезка: {segmentLong.CalculateLength():F2}");
+
+            Console.WriteLine("\nСоздание отрезка с типом double:");
+            Console.WriteLine("Введите x1:");
+            double x1Double = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите y1:");
+            double y1Double = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите x2:");
+            double x2Double = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите y2:");
+            double y2Double = double.Parse(Console.ReadLine());
+            Segment<double> segmentDouble = new Segment<double>(x1Double, y1Double, x2Double, y2Double);
+            segmentDouble.DisplayInfo();
+            Console.WriteLine($"Длина отрезка: {segmentDouble.CalculateLength():F2}");
+
+            // Ожидаем нажатия клавиши для закрытия консоли
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp43/Segment.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp43
+{
+    class Segment<T> where T : struct
+    {
+        private T x1;
+        private T y1;
+        private T x2;
+        private T y2;
+
+        public Segment(T x1, T y1, T x2, T y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+
+        public T X1
+        {
+            get { return x1; }
+            set { x1 = value; }
+        }
+
+        public T Y1
+        {
+            get { return y1; }
+            set { y1 = value; }
+        }
+
+        public T X2
+        {
+            get { return x2; }
+            set { x2 = value; }
+        }
+
+        public T Y2
+        {
+            get { return y2; }
+            set { y2 = value; }
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Отрезок: ({x1}, {y1}) - ({x2}, {y2})");
+        }
+
+        public double CalculateLength()
+        {
+            // Формула длины отрезка: √((x2 - x1)² + (y2 - y1)²)
+            dynamic dx = Convert.ToDouble(x2) - Convert.ToDouble(x1);
+            dynamic dy = Convert.ToDouble(y2) - Convert.ToDouble(y1);
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+    }
+}
