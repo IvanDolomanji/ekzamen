0f2ed69787a4e075fa90568a9944d11e398d8fb3
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2025-06-26 16:11:48 +0300

    Add files via upload

---
+++ ConsoleApp24/Accauntant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    class Accauntant
+    {
+        public bool AskForBonus(Post worker, int hours)
+        {
+            // Получаем требуемое количество часов для данной должности
+            int requiredHours = (int)worker;
+            return hours > requiredHours;
+        }
+    }
+}
